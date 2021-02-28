629a8686b2e6c7dca7efbcf9f9921ac7948a7fc7
Techyy87saleh <79599447+Techyy87saleh@users.noreply.github.com>
2021-02-28 21:41:43 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+   let num    = document.getElementById('zero');
+   let button1 = document.getElementById('increment');
+  let button2  = document.getElementById('decrement');
+    let int = 0;
+  button1.addEventListener('click',(e)=>{
+       e.preventDefault()
+  
+       int = int + 1;
+  
+       num.innerHTML = int;
+       num.value="";
+  });
+  button2.addEventListener('click',(e)=>{
+       e.preventDefault();
+  
+        int = int - 1;
+        num.innerHTML = int;
+        num.value="";
+  
+  });
+   
